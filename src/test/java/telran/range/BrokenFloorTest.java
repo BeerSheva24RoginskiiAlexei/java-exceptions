91122fde1dc298fd0ce467e430d99fa422165de5
AlexeiRoginskii <roginskiial@gmail.com>
2024-08-07 20:56:07 +0300

    fix

--- src/test/java/telran/range/BrokenFloorTest.java
+++ src/test/java/telran/range/BrokenFloorTest.java
@@ -9,16 +9,16 @@ public class BrokenFloorTest {
     private int getMinimalBrokenFloor(BallBrokenFloor bbf) {
         int begin = 0;
         int end = Integer.MAX_VALUE;
-        int mid = (begin + end) / 2;
+        
 
         while (begin <= end) {
+            int mid = (begin + end) / 2;
             try {
                 bbf.checkFloor(mid);
                 begin = mid + 1;
             } catch (Exception e) {
                 end = mid - 1;
             }
-            mid = (begin + end) / 2;
         }
         return begin;
     }
